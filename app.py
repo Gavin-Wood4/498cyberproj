ffb53ae2a76712f1b1a5e74a72bc72bbf81cf23a
Gavin Wood <gavson03@gmail.com>
2025-04-21 15:02:10 -0400

    Add run app statement to app py

--- app.py
+++ app.py
@@ -73,3 +73,6 @@ def handle_password():
 
     print("Redirecting to gmail.com...")
     return redirect("https://gmail.com", code=302)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
