24f04f2c4b140e6c309e3cc02a3128492e6be801
Gavin Wood <gavson03@gmail.com>
2025-04-21 16:31:07 -0400

    Update flask app to run on localhost

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def handle_password():
     return redirect("https://gmail.com", code=302)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
